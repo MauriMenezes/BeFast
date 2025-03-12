50eca15f3f9ba5df5aba1fad2ae014bf77836334
maurilio souza menezes <mauriliosm25@gmail.com>
2025-03-12 16:08:30 -0300

    feat: realicionamento das tabelas

--- BeFast.Infrastructure/Context/AppDbContext .cs
+++ BeFast.Infrastructure/Context/AppDbContext .cs
@@ -10,6 +10,10 @@ namespace BeFast.Infrastructure.Context
 
     public class AppDbContext : DbContext
     {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+        }
+
         public DbSet<User> Users { get; set; }
         public DbSet<Restaurants> Restaurants { get; set; }
         public DbSet<Products> Products { get; set; }
@@ -17,12 +21,43 @@ namespace BeFast.Infrastructure.Context
         public DbSet<Orders> Orders { get; set; }
         public DbSet<OrderItens> OrderItens { get; set; }
 
-        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
-        {
-        }
+
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
+            modelBuilder.Entity<Restaurants>()
+       .HasOne(r => r.User)
+       .WithMany(u => u.Restaurants)
+       .HasForeignKey(r => r.UserId)
+       .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<User>()
+                .HasMany(u => u.CustomerOrders)
+                .WithOne(o => o.Customer)
+                .HasForeignKey(o => o.CustomerId)
+                .OnDelete(DeleteBehavior.Restrict);
+            modelBuilder.Entity<User>()
+                .HasMany(u => u.DeliveryOrders)
+                .WithOne(o => o.DeliveryPerson)
+                .HasForeignKey(o => o.DeliveryPersonId)
+                .OnDelete(DeleteBehavior.Restrict);
             base.OnModelCreating(modelBuilder);
+            modelBuilder.Entity<Orders>()
+                .HasMany(o => o.OrderItens)
+                .WithOne(oi => oi.Order)
+               .HasForeignKey(oi => oi.OrderId)
+              .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<Products>()
+                .HasMany(p => p.OrderItens)
+                .WithOne(oi => oi.Product)
+                .HasForeignKey(oi => oi.ProductId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<Orders>()
+                .HasMany(o => o.Payments)
+                .WithOne(p => p.Order)
+                .HasForeignKey(p => p.OrdersId)
+                .OnDelete(DeleteBehavior.Restrict);
         }
     }
 }
