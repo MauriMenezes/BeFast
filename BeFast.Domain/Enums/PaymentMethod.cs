5bf0bb6468ee563886508b550bc662b3386fe9e0
maurilio souza menezes <mauriliosm25@gmail.com>
2025-03-12 16:11:20 -0300

    feat: adiciona enum com metodos de pagamento

---
+++ BeFast.Domain/Enums/PaymentMethod.cs
@@ -0,0 +1,14 @@
+namespace BeFast.Domain.Enums
+{
+    public enum PaymentMethod
+    {
+        CreditCard,
+        DebitCard,
+        Pix,
+        Cash,
+        BankTransfer,
+        MealVoucher,
+        PayPal,
+        Crypto
+    }
+}
